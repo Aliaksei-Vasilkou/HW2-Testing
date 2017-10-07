f3b64a5c0f64337d3a336fa492f2979d7746d56c
Vasilkov Alexey <coyc@inbox.ru>
2017-10-07 20:45:53 +0300

    Class which implements calculator logic is ready. Next step is create some tests

--- app/src/main/java/coyclab/github/com/hw_testing/Calculator.java
+++ app/src/main/java/coyclab/github/com/hw_testing/Calculator.java
@@ -1,5 +1,36 @@
 package coyclab.github.com.hw_testing;
 
-public class Calculator {
+class Calculator implements ICalculator {
 
+    private int result;
+
+    @Override
+    public String addition(final int x, final int y) {
+        result = x + y;
+        return formatter(result);
+    }
+
+    @Override
+    public String subtraction(final int x, final int y) {
+        result = x - y;
+        return formatter(result);
+    }
+
+    @Override
+    public String multiplication(final int x, final int y) {
+        result = x * y;
+        return formatter(result);
+    }
+
+    @Override
+    public String division(final int x, final int y) {
+        if (y == 0) {
+            throw new ArithmeticException();
+        }
+        return formatter(x / y);
+    }
+
+    private String formatter(final int value) {
+        return String.valueOf(value);
+    }
 }
--- app/src/main/java/coyclab/github/com/hw_testing/ICalculator.java
+++ app/src/main/java/coyclab/github/com/hw_testing/ICalculator.java
@@ -1,5 +1,8 @@
 package coyclab.github.com.hw_testing;
 
-public interface ICalculator {
-
+interface ICalculator {
+    String addition(int x, int y);
+    String subtraction(int x, int y);
+    String multiplication(int x, int y);
+    String division(int x, int y);
 }
