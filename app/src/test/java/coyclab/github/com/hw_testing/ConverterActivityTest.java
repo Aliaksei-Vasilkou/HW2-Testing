ce25328a0a424968249eb2c46d49b8d548ccacbc
Vasilkov Alexey <coyc@inbox.ru>
2017-10-22 14:24:24 +0300

    Added activity lifecycle tests with Robolectric

--- app/src/test/java/coyclab/github/com/hw_testing/ConverterActivityTest.java
+++ app/src/test/java/coyclab/github/com/hw_testing/ConverterActivityTest.java
@@ -1,41 +1,55 @@
 package coyclab.github.com.hw_testing;
 
+import android.app.Activity;
 import android.widget.Button;
 import android.widget.TextView;
+
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class)
+@Config(
+        constants = BuildConfig.class,
+        sdk = 21
+)
 public class ConverterActivityTest {
 
     private Button convertButton;
     private TextView testTextView;
-    private ConverterActivity mActivity;
+    private Activity converterActivity;
 
     @Before
-    public void setUp() throws Exception {
-        mActivity = Robolectric.setupActivity(ConverterActivity.class);
-        convertButton = (Button) mActivity.findViewById(R.id.convert_button);
-        testTextView = (TextView) mActivity.findViewById(R.id.textView);
+    public void setUp() {
+        ActivityController<ConverterActivity> activityController = Robolectric.buildActivity(ConverterActivity.class);
+        activityController.create();
+        activityController.start();
+        activityController.resume();
+
+        converterActivity = activityController.get();
+
+        convertButton = (Button) converterActivity.findViewById(R.id.convert_button);
+        testTextView = (TextView) converterActivity.findViewById(R.id.textView);
     }
 
     @Test
     public void checkActivityNotNull() throws Exception {
-        assertNotNull(mActivity);
+        assertNotNull(converterActivity);
     }
 
     @Test
     public void isTitleCorrect() {
-        assertTrue(mActivity.getTitle().toString().equals("Unit converter"));
+        assertTrue(converterActivity.getTitle().toString().equals("Unit converter"));
     }
 
     @Test
@@ -44,8 +58,8 @@ public class ConverterActivityTest {
     }
 
     @Test
-    public void isConvertButtonDisplay() {
-        assertNotNull(convertButton.getText());
+    public void isConvertButtonEnabled() {
+        assertTrue(convertButton.isEnabled());
     }
 
 }
